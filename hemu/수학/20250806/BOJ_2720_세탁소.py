e4f4e4e213a6edf84f8c9d7f59380ad5281eacfd
Hwamok Lee <ghkahr1890@gmail.com>
2025-08-06 23:44:53 +0900

    [BOJ_11047] 동전/hemu

--- hemu/수학/20250806/BOJ_2720_세탁소.py
+++ hemu/수학/20250806/BOJ_2720_세탁소.py
@@ -1,10 +1,12 @@
 T = int(input())
 for i in range(T):
     C = int(input())
+    #정수로 거스름돈을 받아오므로 quarter등도 정수로 처리
     Q = 25
     D = 10
     N = 5
     P = 1
+    # 제일 큰 단위의 몫은 단위를 남겨줘야할 갯수 나머지는 다음 단위들이 같은 것 반복
     Q_num = C//Q
     D_num = (C%Q)//D
     if (C%Q)%D >= 5:
