930715639641bddf3159470856eb3ca0631374c4
jinhoon <wlsgns326@gmail.com>
2025-08-11 21:40:51 +0900

    [BOJ-2738] 행렬 덧셈 / jxn-hxxn

---
+++ jh/수학/BOJ_2738_행렬_덧셈.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+sum = [[0] * M for _ in range(N)] # 같은 크기의 행렬 생성
+for i in range(N):
+    for j in range(M):
+        sum[i][j] = A[i][j] + B[i][j] # A와 B의 합을 생성한 행렬에 할당
+        print(f'{sum[i][j]} ', end = '') # 같은 줄에 출력
+    print() # 한 줄 띄어 출력
